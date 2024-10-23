5b3cc4d1c9b30ae35931901d8497a5078cefbf98
Abdul Rafay <amirrafay135@gmail.com>
2024-10-23 13:54:28 +0500

    apply the cors

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import  rewriteRoutes from './routes/RewriteText/textRewriteRoutes.js';
+import rewriteRoutes from './routes/RewriteText/textRewriteRoutes.js';
 import userAuthRoutes from './routes/user/auth.js';
 import connectDb from './config/dbConnection.js';
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 connectDb();
 const port = process.env.PORT || 8000;
 
+app.get("/", (req, res) => {
+    res.send('main project backend is working now')
+})
+
 app.use('/api/rewrite', rewriteRoutes);
 
-app.use ('/api/user', userAuthRoutes);
+app.use('/api/user', userAuthRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
