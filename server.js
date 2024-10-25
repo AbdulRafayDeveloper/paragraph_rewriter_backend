6104e3758a72ae1da7a1497bf840166873831662
Abdul Rafay <amirrafay135@gmail.com>
2024-10-25 18:40:41 +0500

    remove cors

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ import dotenv from 'dotenv';
 import rewriteRoutes from './routes/RewriteText/textRewriteRoutes.js';
 import userAuthRoutes from './routes/user/auth.js';
 import connectDb from './config/dbConnection.js';
-import cors from "cors";
+// import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 connectDb();
 const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-    res.send('main project backend is working now')
-})
+// app.get("/", (req, res) => {
+//     res.send('main project backend is working now')
+// })
 
 app.use('/api/rewrite', rewriteRoutes);
 
