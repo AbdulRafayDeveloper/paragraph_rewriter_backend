cc2660116753e13f659f8d70cf66101bf359f9f4
Abdul Rafay <amirrafay135@gmail.com>
2024-11-01 02:48:54 +0500

    Add the collection also

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ import dotenv from 'dotenv';
 import rewriteRoutes from './routes/RewriteText/textRewriteRoutes.js';
 import userAuthRoutes from './routes/user/auth.js';
 import connectDb from './config/dbConnection.js';
+import userRoutes from './routes/user/usersRoutes.js';
+import fileUploadRoutes from './routes/fileUpload/fileUploadRoutes.js';
 import cors from "cors";
+
 
 dotenv.config();
 
